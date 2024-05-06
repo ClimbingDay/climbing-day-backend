89687d8d9131f6f69dbcf71336c77f3bc786a55e
elrdan <seongo9655@gmail.com>
2024-05-06 23:04:46 +0900

    PasswordEncoder 추가

--- core/security/src/main/java/com/climbingday/security/config/SpringSecurityConfig.java
+++ core/security/src/main/java/com/climbingday/security/config/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.climbingday.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +14,12 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
+	/**
+	 * 패스워드 인코더
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
 }
