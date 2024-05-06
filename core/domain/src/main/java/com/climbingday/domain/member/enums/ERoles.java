412ea0bf492182e5d6a4902759df721813b96ba0
elrdan <seongo9655@gmail.com>
2024-05-06 11:57:46 +0900

    Enum ERoles, EStatus 추가

---
+++ core/domain/src/main/java/com/climbingday/domain/member/enums/ERoles.java
@@ -0,0 +1,6 @@
+package com.climbingday.domain.member.enums;
+
+public enum ERoles {
+	ROLE_ADMIN,		// 관리자
+	ROLE_USER		// 회원
+}
---
+++ core/domain/src/main/java/com/climbingday/domain/member/enums/EStatus.java
@@ -0,0 +1,8 @@
+package com.climbingday.domain.member.enums;
+
+public enum EStatus {
+	ACTIVE,			// 활성
+	INACTIVE,		// 비활성
+	DORMANT, 		// 휴면
+	PENDING			// 대기
+}
