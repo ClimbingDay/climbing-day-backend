a2eff502dee1f04535fcf2aabe33fb5640942d44
elrdan <seongo9655@gmail.com>
2024-05-06 11:57:11 +0900

    멤버 엔티티 추가

---
+++ core/domain/src/main/java/com/climbingday/domain/member/Member.java
@@ -0,0 +1,45 @@
+package com.climbingday.domain.member;
+
+import java.sql.Date;
+
+import com.climbingday.domain.member.enums.EStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = "email"),
+	@UniqueConstraint(columnNames = "phoneNum")
+})
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Member {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	private String email;						// 아이디(이메일)
+
+	private String password;					// 비밀번호
+
+	private String name;						// 이름
+
+	private Date birthDate;						// 생년월일
+
+	private String phoneNum;					// 전화번호
+
+	@Enumerated(EnumType.STRING)
+	private EStatus status = EStatus.PENDING;	// 상태
+}
