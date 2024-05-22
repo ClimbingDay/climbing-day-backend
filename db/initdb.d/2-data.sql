b007c67489f7af70d919770ba831b5e66245cb04
elrdan <seongo9655@gmail.com>
2024-05-22 23:32:16 +0900

    data.sql 및 어드민 계정 추가

---
+++ db/initdb.d/2-data.sql
@@ -0,0 +1,3 @@
+-- 관리자 계정
+INSERT INTO member(member_id, email, password, name, birth_date, phone_number, status, roles)
+VALUES(1, 'admin@gmail.com', '1', 'admin', '1111-11-1', '010-000-0000', 'ACTIVE', 'ROLE_ADMIN')
