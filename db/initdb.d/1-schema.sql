2faedb97b5160452bbd1a870a3174dfd1075e71a
elrdan <seongo9655@gmail.com>
2024-05-28 22:21:22 +0900

    데이터 베이스 올릴때 관리자 계정 데이터 추가되도록 수정

--- db/initdb.d/1-schema.sql
+++ db/initdb.d/1-schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE member
 (
-    id                      BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    email                   VARCHAR(30)     NOT NULL,
-    password                VARCHAR(80)     NOT NULL,
-    name                    VARCHAR(20)     NOT NULL,
-    birthDate               DATE            NOT NULL,
-    phoneNumber             VARCHAR(20)     NOT NULL,
-    status                  VARCHAR(20)     NOT NULL,
-    roles                   VARCHAR(20)     NOT NULL
+    member_id                BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email                    VARCHAR(30)     NOT NULL,
+    password                 VARCHAR(80)     NOT NULL,
+    name                     VARCHAR(20)     NOT NULL,
+    birth_date               DATE            NOT NULL,
+    phone_number             VARCHAR(20)     NOT NULL,
+    status                   VARCHAR(20)     NOT NULL,
+    roles                    VARCHAR(20)     NOT NULL
 );
--- db/initdb.d/2-data.sql
+++ db/initdb.d/2-data.sql
@@ -1,3 +1,3 @@
 -- 관리자 계정
 INSERT INTO member(member_id, email, password, name, birth_date, phone_number, status, roles)
-VALUES(1, 'admin@gmail.com', '1', 'admin', '1111-11-1', '010-000-0000', 'ACTIVE', 'ROLE_ADMIN')
+VALUES(1, 'admin@gmail.com', '{bcrypt}$2a$10$RV9gh4kqF7DichT0KphMn.zLGPcdeVC8Gx2yCMjQFtdho.EyIRSWK', 'admin', '1111-11-1', '010-000-0000', 'ACTIVE', 'ROLE_ADMIN')
