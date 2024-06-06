4065986e71888d12571c839c088d40d5eb097bd3
elrdan <seongo9655@gmail.com>
2024-06-06 18:22:19 +0900

    이메일 인증 관련 에러 메시지 추가

--- common/src/main/java/com/climbingday/enums/MailErrorCode.java
+++ common/src/main/java/com/climbingday/enums/MailErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 
 @Getter
 public enum MailErrorCode implements BaseErrorCode {
-	UNABLE_TO_SEND_EMAIL(504, "이메일 전송에 실패했습니다.", HttpStatus.GATEWAY_TIMEOUT);
+	UNABLE_TO_SEND_EMAIL(504, "이메일 전송에 실패했습니다.", HttpStatus.GATEWAY_TIMEOUT),
+	NOT_MATCHED_AUTH_CODE(401, "인증 코드가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED);
+
 
 	private final int errorCode;
 	private final String errorMessage;
