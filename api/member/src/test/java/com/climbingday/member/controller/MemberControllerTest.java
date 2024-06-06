296c4180e3cdd8d114998963e17b11788008f797
elrdan <seongo9655@gmail.com>
2024-06-06 12:03:20 +0900

    실패 테스트 케이스 추가

--- api/member/src/test/java/com/climbingday/member/controller/MemberControllerTest.java
+++ api/member/src/test/java/com/climbingday/member/controller/MemberControllerTest.java
@@ -18,12 +18,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.climbingday.dto.member.MemberLoginDto;
 import com.climbingday.dto.member.MemberRegisterDto;
 import com.climbingday.infra.config.TestConfig;
-import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
 
 import io.restassured.RestAssured;
@@ -52,7 +50,6 @@ class MemberControllerTest extends TestConfig {
 
 	@Test
 	@DisplayName("1. 회원가입 테스트")
-	@Transactional
 	public void memberRegisterTest() throws Exception {
 		MemberRegisterDto registerDto = MemberRegisterDto.builder()
 			.email("test@naver.com")
@@ -64,7 +61,7 @@ class MemberControllerTest extends TestConfig {
 			.build();
 
 		given(spec).log().all()
-			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API",
+			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API - 성공",
 				RestAssuredRestDocumentationWrapper.resourceDetails()
 					.tag("회원 API")
 					.summary("회원 가입"),
@@ -90,8 +87,114 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("2. 로그인 테스트")
+	@DisplayName("2. 회원가입 중복 회원 실패 테스트")
+	public void duplicatedMemberRegisterTest() throws Exception {
+		MemberRegisterDto registerDto = MemberRegisterDto.builder()
+			.email("test@naver.com")
+			.name("test")
+			.password("123456")
+			.passwordConfirm("123456")
+			.phoneNumber("010-1234-5678")
+			.birthDate("2000-11-11")
+			.build();
+
+		given(spec).log().all()
+			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API - 실패: 중복 회원",
+				RestAssuredRestDocumentationWrapper.resourceDetails()
+					.tag("회원 API")
+					.summary("회원 가입"),
+				requestFields(
+					fieldWithPath("email").type(STRING).description("이메일(계정)"),
+					fieldWithPath("name").type(STRING).description("이름"),
+					fieldWithPath("password").type(STRING).description("패스워드"),
+					fieldWithPath("passwordConfirm").type(STRING).description("패스워드 확인"),
+					fieldWithPath("phoneNumber").type(STRING).description("핸드폰 번호"),
+					fieldWithPath("birthDate").type(STRING).description("생년월일")
+				),
+				responseFields(
+					fieldWithPath("errorCode").type(NUMBER).description("상태 코드"),
+					fieldWithPath("errorMessage").type(STRING).description("상태 메시지")
+				)))
+			.contentType(JSON)
+			.body(registerDto)
+			.when()
+				.post("/v1/member/register")
+			.then().log().all()
+				.statusCode(409);
+	}
+
+	@Test
+	@DisplayName("3. 회원가입 입력값 유효성 실패 테스트")
+	public void validMemberRegisterTest() throws Exception {
+		MemberRegisterDto registerDto = MemberRegisterDto.builder()
+			.email("testnaver.com")
+			.name(null)
+			.password("12345")
+			.passwordConfirm("12345")
+			.phoneNumber("0101234-5678")
+			.birthDate("200011-11")
+			.build();
+
+		given(spec).log().all()
+			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API - 실패: 입력 유효성 체크",
+				RestAssuredRestDocumentationWrapper.resourceDetails()
+					.tag("회원 API")
+					.summary("회원 가입"),
+				requestFields(
+					fieldWithPath("email").type(STRING).description("이메일(계정)"),
+					fieldWithPath("name").type(STRING).description("이름"),
+					fieldWithPath("password").type(STRING).description("패스워드"),
+					fieldWithPath("passwordConfirm").type(STRING).description("패스워드 확인"),
+					fieldWithPath("phoneNumber").type(STRING).description("핸드폰 번호"),
+					fieldWithPath("birthDate").type(STRING).description("생년월일")
+				),
+				responseFields(
+					fieldWithPath("errorCode").type(NUMBER).description("상태 코드"),
+					fieldWithPath("errorMessage").type(STRING).description("상태 메시지"),
+					subsectionWithPath("validation").type(OBJECT).description("유효하지 않은 필드")
+				)))
+			.contentType(JSON)
+			.body(registerDto)
+			.when()
+				.post("/v1/member/register")
+			.then().log().all()
+				.statusCode(400);
+	}
+
+	@Test
+	@DisplayName("4. 로그인 테스트")
 	public void memberLoginTest() throws Exception {
+		MemberLoginDto memberLoginDto = MemberLoginDto.builder()
+			.email("test@naver.com")
+			.password("123456")
+			.build();
+
+		given(spec).log().all()
+			.filter(RestAssuredRestDocumentationWrapper.document("로그인 API - 성공",
+				RestAssuredRestDocumentationWrapper.resourceDetails()
+					.tag("회원 API")
+					.summary("로그인"),
+				requestFields(
+					fieldWithPath("email").type(STRING).description("이메일(계정)"),
+					fieldWithPath("password").type(STRING).description("패스워드")
+				),
+				responseFields(
+					fieldWithPath("code").type(NUMBER).description("상태 코드"),
+					fieldWithPath("message").type(STRING).description("상태 메시지"),
+					fieldWithPath("data.accessToken").type(STRING).description("액세스 토큰"),
+					fieldWithPath("data.refreshToken").type(STRING).description("리프래쉬 토큰")
+				)))
+			.contentType(JSON)
+			.body(memberLoginDto)
+		.when()
+			.post("/v1/member/login")
+		.then().log().all()
+			.statusCode(200);
+	}
+
+	@Test
+	@DisplayName("5. 로그인 테스트")
+	public void memberLoginNotMatchTest() throws Exception {
 		MemberLoginDto memberLoginDto = MemberLoginDto.builder()
 			.email("test@naver.com")
 			.password("123456")
@@ -114,9 +217,36 @@ class MemberControllerTest extends TestConfig {
 				)))
 			.contentType(JSON)
 			.body(memberLoginDto)
-		.when()
+			.when()
 			.post("/v1/member/login")
-		.then().log().all()
+			.then().log().all()
 			.statusCode(200);
 	}
+
+	// @Test
+	// @DisplayName("4. 이메일 인증 요청 테스트")
+	// public void memberEmailAuthRequestTest() throws Exception {
+	// 	EmailAuthDto emailAuthDto = EmailAuthDto.builder()
+	// 		.email("seongo0521@gmail.com")
+	// 		.build();
+	//
+	// 	given(spec).log().all()
+	// 		.filter(RestAssuredRestDocumentationWrapper.document("이메일 인증 요청 API",
+	// 			RestAssuredRestDocumentationWrapper.resourceDetails()
+	// 				.tag("회원 API")
+	// 				.summary("이메일 인증 요청"),
+	// 			requestFields(
+	// 				fieldWithPath("email").type(STRING).description("이메일(계정)")
+	// 			),
+	// 			responseFields(
+	// 				fieldWithPath("code").type(NUMBER).description("상태 코드"),
+	// 				fieldWithPath("message").type(STRING).description("상태 메시지")
+	// 			)))
+	// 		.contentType(JSON)
+	// 		.body(emailAuthDto)
+	// 	.when()
+	// 		.post("http://localhost:8089/v1/member/email/auth/request")
+	// 	.then().log().all()
+	// 		.statusCode(200);
+	// }
 }
