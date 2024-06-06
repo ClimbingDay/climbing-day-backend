369af90559d204b963a692fe6ad081187af89ba5
elrdan <seongo9655@gmail.com>
2024-06-06 12:49:21 +0900

    회원 로그인 테스트 케이스 수정

--- api/member/src/test/java/com/climbingday/member/controller/MemberControllerTest.java
+++ api/member/src/test/java/com/climbingday/member/controller/MemberControllerTest.java
@@ -49,7 +49,7 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("1. 회원가입 테스트")
+	@DisplayName("1-1. 회원가입 테스트")
 	public void memberRegisterTest() throws Exception {
 		MemberRegisterDto registerDto = MemberRegisterDto.builder()
 			.email("test@naver.com")
@@ -87,8 +87,8 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("2. 회원가입 중복 회원 실패 테스트")
-	public void duplicatedMemberRegisterTest() throws Exception {
+	@DisplayName("1-2. 회원가입 중복 회원 실패 테스트")
+	public void memberRegisterDuplicatedTest() throws Exception {
 		MemberRegisterDto registerDto = MemberRegisterDto.builder()
 			.email("test@naver.com")
 			.name("test")
@@ -124,8 +124,8 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("3. 회원가입 입력값 유효성 실패 테스트")
-	public void validMemberRegisterTest() throws Exception {
+	@DisplayName("1-3. 회원가입 입력값 유효성 실패 테스트")
+	public void memberRegisterValidTest() throws Exception {
 		MemberRegisterDto registerDto = MemberRegisterDto.builder()
 			.email("testnaver.com")
 			.name("")
@@ -136,7 +136,7 @@ class MemberControllerTest extends TestConfig {
 			.build();
 
 		given(spec).log().all()
-			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API - 실패: 입력 유효성 체크",
+			.filter(RestAssuredRestDocumentationWrapper.document("회원 가입 API - 실패: 필드 유효성 체크",
 				RestAssuredRestDocumentationWrapper.resourceDetails()
 					.tag("회원 API")
 					.summary("회원 가입"),
@@ -162,7 +162,7 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("4. 로그인 테스트")
+	@DisplayName("2-1. 로그인 테스트")
 	public void memberLoginTest() throws Exception {
 		MemberLoginDto memberLoginDto = MemberLoginDto.builder()
 			.email("test@naver.com")
@@ -193,15 +193,15 @@ class MemberControllerTest extends TestConfig {
 	}
 
 	@Test
-	@DisplayName("5. 로그인 테스트")
+	@DisplayName("2-2. 로그인 아이디 혹은 패스워드 실패 테스트")
 	public void memberLoginNotMatchTest() throws Exception {
 		MemberLoginDto memberLoginDto = MemberLoginDto.builder()
 			.email("test@naver.com")
-			.password("123456")
+			.password("1234567")
 			.build();
 
 		given(spec).log().all()
-			.filter(RestAssuredRestDocumentationWrapper.document("로그인 API",
+			.filter(RestAssuredRestDocumentationWrapper.document("로그인 API - 실패: 로그인 정보",
 				RestAssuredRestDocumentationWrapper.resourceDetails()
 					.tag("회원 API")
 					.summary("로그인"),
@@ -210,17 +210,45 @@ class MemberControllerTest extends TestConfig {
 					fieldWithPath("password").type(STRING).description("패스워드")
 				),
 				responseFields(
-					fieldWithPath("code").type(NUMBER).description("상태 코드"),
-					fieldWithPath("message").type(STRING).description("상태 메시지"),
-					fieldWithPath("data.accessToken").type(STRING).description("액세스 토큰"),
-					fieldWithPath("data.refreshToken").type(STRING).description("리프래쉬 토큰")
+					fieldWithPath("errorCode").type(NUMBER).description("상태 코드"),
+					fieldWithPath("errorMessage").type(STRING).description("상태 메시지")
 				)))
 			.contentType(JSON)
 			.body(memberLoginDto)
 			.when()
 			.post("/v1/member/login")
 			.then().log().all()
-			.statusCode(200);
+			.statusCode(401);
+	}
+
+	@Test
+	@DisplayName("2-3. 로그인 테스트")
+	public void memberLoginValidTest() throws Exception {
+		MemberLoginDto memberLoginDto = MemberLoginDto.builder()
+			.email("testnaver.com")
+			.password("12345")
+			.build();
+
+		given(spec).log().all()
+			.filter(RestAssuredRestDocumentationWrapper.document("로그인 API - 실패: 필드 유효성 체크",
+				RestAssuredRestDocumentationWrapper.resourceDetails()
+					.tag("회원 API")
+					.summary("로그인"),
+				requestFields(
+					fieldWithPath("email").type(STRING).description("이메일(계정)"),
+					fieldWithPath("password").type(STRING).description("패스워드")
+				),
+				responseFields(
+					fieldWithPath("errorCode").type(NUMBER).description("상태 코드"),
+					fieldWithPath("errorMessage").type(STRING).description("상태 메시지"),
+					subsectionWithPath("validation").type(OBJECT).description("유효하지 않은 필드")
+				)))
+			.contentType(JSON)
+			.body(memberLoginDto)
+			.when()
+			.post("/v1/member/login")
+			.then().log().all()
+			.statusCode(400);
 	}
 
 	// @Test
--- common/src/main/java/com/climbingday/enums/MemberErrorCode.java
+++ common/src/main/java/com/climbingday/enums/MemberErrorCode.java
@@ -8,11 +8,10 @@ import lombok.Getter;
 
 @Getter
 public enum MemberErrorCode implements BaseErrorCode {
-	NOT_FIND_MEMBER_EMAIL(400, "존재하지 않는 회원입니다.", HttpStatus.BAD_REQUEST),
 	DUPLICATED_MEMBER_EMAIL(409, "이미 존재하는 회원 이메일입니다.", HttpStatus.CONFLICT),
 	DUPLICATED_MEMBER_PHONE_NUMBER(409, "이미 등록된 휴대폰 번호입니다.", HttpStatus.CONFLICT),
 	DELETE_MEMBER(400, "탈퇴 또는 삭제된 회원입니다.", HttpStatus.BAD_REQUEST),
-	CHECK_ID_OR_PASSWORD(400, "아이디 또는 비밀번호를 확인해주세요.", HttpStatus.BAD_REQUEST),
+	CHECK_ID_OR_PASSWORD(401, "아이디 또는 비밀번호를 확인해주세요.", HttpStatus.UNAUTHORIZED),
 	PASSWORD_NOT_MATCHED(400, "비밀번호와 비밀번호 확인이 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
 
 	private final int errorCode;
