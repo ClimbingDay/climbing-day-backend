51466ae72f4de56fc06ef625f1a25c82b1b34bfa
elrdan <seongo9655@gmail.com>
2024-06-06 18:21:33 +0900

    EmailAuthDto authCode 필드 추가

--- common/src/main/java/com/climbingday/dto/member/EmailAuthDto.java
+++ common/src/main/java/com/climbingday/dto/member/EmailAuthDto.java
@@ -15,4 +15,6 @@ public class EmailAuthDto {
 	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "유효하지 않은 이메일 형식입니다.")
 	@NotBlank(message = "이메일은 필수 항목입니다.")
 	private String email;
+
+	private String authCode;
 }
