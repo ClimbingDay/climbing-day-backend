e94359515acc0fc251fa91aed8f9e7bc24558837
elrdan <seongo9655@gmail.com>
2024-06-07 01:18:11 +0900

    생성 관련 필드, 수정 관련 필드를 담당하는 Entity 추가

---
+++ core/domain/src/main/java/com/climbingday/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.climbingday.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+	@CreatedBy
+	@Column(updatable = false)
+	private String createdBy; 			// 생성자
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;	// 생성일
+}
