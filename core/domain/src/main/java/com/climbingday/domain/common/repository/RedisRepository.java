671dc871582b8e562871fa6dd8bf93c45e30c3d8
elrdan <seongo9655@gmail.com>
2024-06-06 18:20:35 +0900

    만료시간 설정 함수 분리, redis null check 추가

--- core/domain/src/main/java/com/climbingday/domain/common/repository/RedisRepository.java
+++ core/domain/src/main/java/com/climbingday/domain/common/repository/RedisRepository.java
@@ -1,12 +1,17 @@
 package com.climbingday.domain.common.repository;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import static com.climbingday.enums.RedisErrorCode.*;
 
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
+
+import com.climbingday.domain.exception.CustomRedisException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,23 +19,64 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisRepository {
 
+	@Value("${spring.mail.auth-code-expiration-millis}")
+	private int authCodeSeconds;
+
 	private final RedisTemplate<String, Object> redisTemplate;
 
 	/**
-	 * refresh token 저장
+	 * SET 이메일 인증코드
 	 */
-	public void setRedisRefreshToken(Long memberId, String refreshToken, int expirationSeconds) {
+	public void setEmailCodeAndConfirm(String email, String authCode, boolean confirm) {
 		ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-		Duration expireDuration = Duration.ofSeconds(expirationSeconds);
-		valueOperations.set(String.valueOf(memberId), Map.of("refreshToken", refreshToken), expireDuration);
+		valueOperations.set(email,
+			Map.of(
+				"authCode", authCode,
+				"confirm", confirm
+			), createExpireDuration(authCodeSeconds));
 	}
 
 	/**
-	 * refresh token 가져오기
+	 * GET 이메일 인증코드
+	 */
+	public Map getEmailCode(String email) {
+		return getKeyIfPresent(email);
+	}
+
+	/**
+	 * SET refresh token
+	 */
+	public void setRedisRefreshToken(Long memberId, String refreshToken, int expirationSeconds) {
+		ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set(String.valueOf(memberId), Map.of("refreshToken", refreshToken), createExpireDuration(expirationSeconds));
+	}
+
+	/**
+	 * GET refresh token
 	 */
 	public Object getRefreshToken(Long memberId) {
 		return redisTemplate
 			.opsForValue()
 			.get(String.valueOf(memberId));
+	}
+
+	/**
+	 * 만료 시간 생성
+	 */
+	private Duration createExpireDuration(int seconds) {
+		return Duration.ofSeconds(seconds);
+	}
+
+	/**
+	 * redis key null 체크
+	 */
+	private Map getKeyIfPresent(String key) {
+		Optional<Object> optionalResult = Optional.ofNullable(redisTemplate.opsForValue().get(key));
+
+		if(optionalResult.isPresent()) {
+			return (Map)optionalResult.get();
+		}else {
+			throw new CustomRedisException(REDIS_EMPTY_KEY);
+		}
 	}
 }
