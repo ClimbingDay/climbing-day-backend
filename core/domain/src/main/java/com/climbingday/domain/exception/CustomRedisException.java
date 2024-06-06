6e9811590ecf1235d1fe757ca5fd5852820fe0a9
elrdan <seongo9655@gmail.com>
2024-06-06 18:21:17 +0900

    RedisException 추가

---
+++ core/domain/src/main/java/com/climbingday/domain/exception/CustomRedisException.java
@@ -0,0 +1,15 @@
+package com.climbingday.domain.exception;
+
+import com.climbingday.enums.BaseErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class CustomRedisException extends RuntimeException {
+	private final BaseErrorCode errorCode;
+
+	public CustomRedisException(BaseErrorCode errorCode) {
+		super(errorCode.getErrorMessage());
+		this.errorCode = errorCode;
+	}
+}
