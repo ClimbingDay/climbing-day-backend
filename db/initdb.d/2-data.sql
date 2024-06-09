e6f4d48ca84555891e0f18ebe8f6b1c9a05ffcc8
elrdan <163220957+elrdan@users.noreply.github.com>
2024-06-09 20:52:56 +0900

    Delete db/initdb.d directory

--- db/initdb.d/2-data.sql
+++
@@ -1,3 +0,0 @@
--- 관리자 계정
-INSERT INTO member(member_id, email, password, name, birth_date, phone_number, status, roles, created_date)
-VALUES(1, 'admin@gmail.com', '{bcrypt}$2a$10$RV9gh4kqF7DichT0KphMn.zLGPcdeVC8Gx2yCMjQFtdho.EyIRSWK', 'admin', '1111-11-1', '010-000-0000', 'ACTIVE', 'ROLE_ADMIN', CURRENT_DATE)
