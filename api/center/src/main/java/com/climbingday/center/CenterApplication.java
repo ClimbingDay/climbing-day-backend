81af537a16ba64bb0ad7c85eb6f6cd84fa31e3d2
elrdan <seongo9655@gmail.com>
2024-06-09 22:30:37 +0900

    api/center 설정 추가

---
+++ api/center/src/main/java/com/climbingday/center/CenterApplication.java
@@ -0,0 +1,14 @@
+package com.climbingday.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.climbingday"})
+public class CenterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CenterApplication.class, args);
+	}
+}
