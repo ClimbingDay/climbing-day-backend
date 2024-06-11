b2e76b7f91a233005b5612014f294b57452b9dc0
elrdan <seongo9655@gmail.com>
2024-06-11 20:52:06 +0900

    Center 등록 Dto 추가

---
+++ common/src/main/java/com/climbingday/dto/center/CenterRegisterDto.java
@@ -0,0 +1,45 @@
+package com.climbingday.dto.center;
+
+import java.time.LocalTime;
+
+import com.climbingday.annotation.LocalTimeValid;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class CenterRegisterDto {
+	@NotBlank(message = "암장 이름은 필수 항목입니다.")
+	@Size(max = 40, message = "암장 이름은 40자 이하로 입력해주세요.")
+	private String name;
+
+	@Pattern(regexp = "^(0(?:2|3[1-3]|4[1-4]|5[1-5]|6[1-4])-[1-9]\\d{2,3}-\\d{4}|01[016789]-[1-9]\\d{2,3}-\\d{4})$",
+		message = "전화번호를 올바르게 입력해주세요")
+	@NotBlank(message = "전화번호는 필수 항목입니다.")
+	private String phoneNum;
+
+	@NotBlank(message = "주소는 필수 항목입니다.")
+	private String address;
+
+	@NotBlank(message = "위도는 필수 항목입니다.")
+	private double latitude;
+
+	@NotBlank(message = "경도는 필수 항목입니다.")
+	private double longitude;
+
+	@LocalTimeValid(message = "영업 시작 시간을 올바르게 입력해주세요.")
+	private LocalTime openTime;
+
+	@LocalTimeValid(message = "영업 종료 시간을 올바르게 입력해주세요.")
+	private LocalTime closeTime;
+
+	private String description;
+
+	private String notice;
+}
