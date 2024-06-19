a3c702ccd725d1171790016e07160327b0d70c5b
elrdan <seongo9655@gmail.com>
2024-06-19 22:09:31 +0900

    docker 파일 수정

--- core/infra/src/main/java/com/climbingday/infra/config/JasyptConfig.java
+++ core/infra/src/main/java/com/climbingday/infra/config/JasyptConfig.java
@@ -1,13 +1,9 @@
 package com.climbingday.infra.config;
 
-import java.util.Map;
-
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.climbingday.utils.DotenvUtil;
 
 @Configuration
 public class JasyptConfig {
@@ -15,9 +11,8 @@ public class JasyptConfig {
 	@Bean
 	public StringEncryptor stringEncryptor() {
 		// 최상단 디렉토리 .env 파일
-		Map<String, String> env = DotenvUtil.loadEnv();
-		String password = env.get("JASYPT_ENCRYPTOR_PASSWORD");
-		String algorithm = env.get("JASYPT_ALGORITHM");
+		String password = System.getenv("JASYPT_ENCRYPTOR_PASSWORD");
+		String algorithm = System.getenv("JASYPT_ALGORITHM");
 
 		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
 		encryptor.setPassword(password);
