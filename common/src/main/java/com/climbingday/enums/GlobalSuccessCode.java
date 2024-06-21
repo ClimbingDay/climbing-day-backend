a14d522d45c93ae15898c43bced2068609005ffc
elrdan <seongo9655@gmail.com>
2024-06-21 22:34:58 +0900

    성공 코드 메시지 수정

--- common/src/main/java/com/climbingday/enums/GlobalSuccessCode.java
+++ common/src/main/java/com/climbingday/enums/GlobalSuccessCode.java
@@ -10,7 +10,7 @@ public enum GlobalSuccessCode {
 	EMAIL_SEND_SUCCESS(200, "인증 메일이 정상적으로 발송되었습니다.", HttpStatus.OK),
 	EMAIL_AUTH_CONFIRM(200, "이메일 인증이 정상적으로 처리되었습니다.", HttpStatus.OK),
 	AVAILABLE_EMAIL(200, "사용이 가능한 이메일입니다.", HttpStatus.OK),
-	AVAILABLE_PHONE_NUM(200, "사용이 가능한 전화번호입니다.", HttpStatus.OK),
+	AVAILABLE_PHONE_NUM(200, "사용이 가능한 휴대폰번호입니다.", HttpStatus.OK),
 	AVAILABLE_NICK_NAME(200, "사용이 가능한 닉네임입니다.", HttpStatus.OK),
 	CREATE(201, "정상적으로 생성되었습니다.", HttpStatus.CREATED);
 
