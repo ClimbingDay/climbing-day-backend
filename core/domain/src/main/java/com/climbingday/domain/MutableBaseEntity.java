f0f786127e28870cfe17c8cc07118cf01e4b73e7
elrdan <seongo9655@gmail.com>
2024-06-23 15:41:35 +0900

    공백 제거

--- core/domain/src/main/java/com/climbingday/domain/MutableBaseEntity.java
+++ core/domain/src/main/java/com/climbingday/domain/MutableBaseEntity.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class MutableBaseEntity extends BaseEntity{
