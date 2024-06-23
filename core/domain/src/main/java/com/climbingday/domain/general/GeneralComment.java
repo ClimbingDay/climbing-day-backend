e95ab1fabeeeaaaa6bd01ab714b2199613fc4c57
elrdan <seongo9655@gmail.com>
2024-06-23 15:42:02 +0900

    일반 게시글 댓글 Entity 생성

---
+++ core/domain/src/main/java/com/climbingday/domain/general/GeneralComment.java
@@ -0,0 +1,53 @@
+package com.climbingday.domain.general;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.climbingday.domain.member.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder @NoArgsConstructor @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class GeneralComment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "general_comment_id")
+	private Long id;							// 일반 게시글 댓글 id
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "general_post_id")
+	private GeneralPost generalPost;			// 일반 게시글
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;						// 회원
+
+	@Lob
+	private String content;						// 일반 게시글 내용
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;			// 생성일
+
+	@LastModifiedDate
+	private LocalDateTime updatedDate; 			// 수정일
+}
