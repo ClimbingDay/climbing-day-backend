5b0a10e9c0ac40eeef5b19e909951235e24d31ce
elrdan <seongo9655@gmail.com>
2024-07-04 18:25:31 +0900

    약관 Entity 생성

---
+++ core/domain/src/main/java/com/climbingday/domain/terms/Terms.java
@@ -0,0 +1,31 @@
+package com.climbingday.domain.terms;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Terms {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String type;
+
+	private String version;
+
+	private String contentUrl;
+
+	private boolean isMandatory;
+
+	private LocalDateTime createDate;
+}
