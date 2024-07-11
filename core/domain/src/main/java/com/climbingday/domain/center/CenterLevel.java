3319cf4ad7e0a7d12a625ca98be52277c93cd358
elrdan <seongo9655@gmail.com>
2024-07-11 19:17:03 +0900

    centerLevel Entity 생성

---
+++ core/domain/src/main/java/com/climbingday/domain/center/CenterLevel.java
@@ -0,0 +1,33 @@
+package com.climbingday.domain.center;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class CenterLevel {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "center_lavel_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "center_id")
+	private Center center;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "level_id")
+	private Level level;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "color_id")
+	private Color color;
+}
