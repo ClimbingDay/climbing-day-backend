6059e4e5d38b9b72f940bd5c1204890af2a75cc8
elrdan <seongo9655@gmail.com>
2024-07-13 04:53:36 +0900

    ErrorMessage 수정

--- common/src/main/java/com/climbingday/enums/GlobalSuccessCode.java
+++ common/src/main/java/com/climbingday/enums/GlobalSuccessCode.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 
 @Getter
 public enum GlobalSuccessCode {
-	SUCCESS(200, "정상 처리되었습니다.", HttpStatus.OK),
+	SUCCESS(200, "정상적으로 처리되었습니다.", HttpStatus.OK),
 	PASSWORD_RESET_SUCCESS(200, "비밀번호 변경이 정상적으로 처리되었습니다.", HttpStatus.OK),
 	EMAIL_SEND_SUCCESS(200, "인증 메일이 정상적으로 발송되었습니다.", HttpStatus.OK),
+	MEMBER_PROFILE_UPDATE_SUCCESS(200, "프로필 수정이 정상적으로 처리되었습니다.", HttpStatus.OK),
 	EMAIL_AUTH_CONFIRM(200, "이메일 인증이 정상적으로 처리되었습니다.", HttpStatus.OK),
 	AVAILABLE_EMAIL(200, "사용이 가능한 이메일입니다.", HttpStatus.OK),
 	AVAILABLE_PHONE_NUM(200, "사용이 가능한 휴대폰번호입니다.", HttpStatus.OK),
