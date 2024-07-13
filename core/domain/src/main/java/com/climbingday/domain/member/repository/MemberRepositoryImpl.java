e503cce9fa6e99e6b9cdde7f7529574e843afa20
elrdan <seongo9655@gmail.com>
2024-07-14 08:33:24 +0900

    매칭 쿼리 추가

--- core/domain/src/main/java/com/climbingday/domain/member/repository/MemberRepositoryImpl.java
+++ core/domain/src/main/java/com/climbingday/domain/member/repository/MemberRepositoryImpl.java
@@ -9,9 +9,14 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.climbingday.dto.member.MemberDto;
+import com.climbingday.dto.member.MemberMatchDto;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -29,7 +34,7 @@ public class MemberRepositoryImpl implements MemberCustom {
 				member.email,
 				member.status,
 				member.roles
-				))
+			))
 			.from(member)
 			.fetch();
 	}
@@ -50,6 +55,43 @@ public class MemberRepositoryImpl implements MemberCustom {
 			.leftJoin(memberCrew).on(member.id.eq(memberCrew.member.id))
 			.leftJoin(crew).on(memberCrew.crew.id.eq(crew.id))
 			.where(memberId(id))
+			.fetch();
+	}
+
+	@Override
+	public List<MemberMatchDto> getMemberByDistance(double myLat, double myLon, double distance, int limit,
+		Long memberId) {
+		// 지구 반지름(킬로미터 단위)
+		double radius = 6371;
+
+		// Haversine 공식 사용: 두 지점 간의 거리를 구하는 공식
+		NumberExpression<Double> haversine = Expressions.numberTemplate(Double.class,
+			"{0} * acos(cos(radians({1})) * cos(radians({2})) * cos(radians({3}) - radians({4})) + sin(radians({1})) * sin(radians({2})))",
+			radius, myLat, member.latitude, member.longitude, myLon);
+
+		// 랜덤 정렬을 위한 Expressions 생성
+		OrderSpecifier<?> randomOrder = Expressions.numberTemplate(Double.class, "function('RAND')").asc();
+
+		return queryFactory
+			.select(Projections.fields(MemberMatchDto.class,
+				member.nickName,
+				new CaseBuilder()
+					.when(member.memberSettings.isProfileVisible.isFalse())
+					.then("https://climbing-day-bucket.s3.ap-northeast-2.amazonaws.com/climbing-day-no-image.jpg")
+					.otherwise(member.profileImage).as("profileImage")
+			))
+			.from(member)
+			.where(
+				member.memberSettings.isMatchingEnabled.isTrue() // 매칭이 활성화된 경우
+					.and(
+						member.id.ne(memberId)
+					)
+					.and(
+						haversine.loe(distance) // haversine 거리가 지정된 거리보다 작거나 같음
+					)
+			)
+			.orderBy(randomOrder) // 랜덤으로 정렬
+			.limit(limit) // 제한 수
 			.fetch();
 	}
 
