56e6622a11cc241887d4aadf029bf7bfc7045406
elrdan <seongo9655@gmail.com>
2024-07-14 09:36:22 +0900

    매칭 실패 에러코드 추가

--- common/src/main/java/com/climbingday/enums/EventErrorCode.java
+++ common/src/main/java/com/climbingday/enums/EventErrorCode.java
@@ -10,7 +10,8 @@ import lombok.Getter;
 public enum EventErrorCode implements BaseErrorCode {
 	UNABLE_TO_SEND_EMAIL(504, "이메일 전송에 실패했습니다.", HttpStatus.GATEWAY_TIMEOUT),
 	NOT_MATCHED_AUTH_CODE(401, "인증 코드가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
-	NOT_AUTHENTICATED_EMAIL(400, "이메일 인증이 되지 않았습니다. 이메일 인증 후 다시 시도해 주세요.", HttpStatus.BAD_REQUEST);
+	NOT_AUTHENTICATED_EMAIL(400, "이메일 인증이 되지 않았습니다. 이메일 인증 후 다시 시도해 주세요.", HttpStatus.BAD_REQUEST),
+	NOT_FIND_MATCHING_MEMBER(404, "현재 위치에 매칭 회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
 
 
 	private final int errorCode;
