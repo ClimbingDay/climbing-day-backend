4d308f97d29f7466db719ec7da7d597ec53864c8
elrdan <seongo9655@gmail.com>
2024-07-28 08:21:22 +0900

    AllCenterLevel 조회 쿼리 추가

--- core/domain/src/main/java/com/climbingday/domain/center/repository/CenterLevelRepository.java
+++ core/domain/src/main/java/com/climbingday/domain/center/repository/CenterLevelRepository.java
@@ -1,9 +1,11 @@
 package com.climbingday.domain.center.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.climbingday.domain.center.CenterLevel;
 
 public interface CenterLevelRepository extends JpaRepository<CenterLevel, Long>, CenterLevelCustom {
-
+	List<CenterLevel> findAllByCenterIdOrderByLevelIdAsc(Long centerId);
 }
