1cb56e192550f630856ded92518714caead541b9
elrdan <seongo9655@gmail.com>
2024-07-28 08:22:14 +0900

    NOT EXISTS CENTER LEVEL ErrorCode 추가

--- common/src/main/java/com/climbingday/enums/EventErrorCode.java
+++ common/src/main/java/com/climbingday/enums/EventErrorCode.java
@@ -11,7 +11,8 @@ public enum EventErrorCode implements BaseErrorCode {
 	UNABLE_TO_SEND_EMAIL(504, "이메일 전송에 실패했습니다.", HttpStatus.GATEWAY_TIMEOUT),
 	NOT_MATCHED_AUTH_CODE(401, "인증 코드가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
 	NOT_AUTHENTICATED_EMAIL(400, "이메일 인증이 되지 않았습니다. 이메일 인증 후 다시 시도해 주세요.", HttpStatus.BAD_REQUEST),
-	NOT_FIND_MATCHING_MEMBER(404, "현재 위치에 매칭 회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+	NOT_FIND_MATCHING_MEMBER(404, "현재 위치에 매칭 회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+	NOT_EXISTS_CENTER_LEVEL(400, "존재하지 않는 레벨이 포함되어 있습니다.", HttpStatus.BAD_REQUEST);
 
 
 	private final int errorCode;
